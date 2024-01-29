faa040777516d6c112258601fc1f18a6c222f988
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2024-01-29 10:25:12 +0100

    Add route jobs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur mollitia
+      accusantium, quibusdam voluptatibus amet repellendus obcaecati atque.
+      Harum amet laboriosam cum magnam possimus esse iusto odio aliquid, numquam
+      sed. Earum, quaerat? Delectus fugit maiores eius laborum odit!
+      Repellendus, doloribus consequuntur ullam modi corrupti, at, velit
+      delectus corporis quisquam expedita maxime!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+      consectetur tempora voluptatibus ad vitae quidem accusamus sunt delectus
+      reiciendis amet dolores quos nisi cumque, voluptas soluta totam qui odio
+      quo nemo perferendis? Commodi cumque maiores consectetur ipsam iste.
+      Voluptatum maxime veniam cupiditate deserunt voluptate hic illo tempora
+      nihil unde? Fuga.
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+  components: {},
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste optio vitae
+      laboriosam ex illo pariatur tenetur esse. Neque expedita nisi alias ipsam
+      animi eveniet fugiat quisquam dolore rem? Quod facilis accusamus ipsam
+      quibusdam minus temporibus delectus alias adipisci, repellat id amet
+      consequatur officiis consequuntur! Minus perspiciatis dolore ad cumque
+      quia pariatur tempora fugit, illo modi asperiores incidunt. Iusto magnam,
+      recusandae placeat tempora minus molestias consequatur blanditiis quod
+      optio iure quaerat quia labore rerum amet cupiditate dolores velit dicta
+      voluptates similique voluptatem nulla ab. Eveniet labore voluptate vel
+      quidem! Aspernatur iusto porro quia natus in libero quibusdam inventore
+      autem corporis illo?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis culpa
+      quibusdam illum, itaque commodi officia dicta consectetur provident,
+      numquam voluptatibus repellat eos sit iusto optio veniam sequi quos.
+      Temporibus, mollitia pariatur veritatis quaerat vero, quam quo
+      perspiciatis laborum aliquam neque suscipit illum alias ducimus eligendi
+      quos eos exercitationem illo cumque.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
